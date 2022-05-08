ecc2d4cbc64c7fe5447fd6caecfb1c997201878c
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-09 05:22:05 +0800

    Mi439: CipherOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_Mi439-user \
     lineage_Mi439-userdebug \
     lineage_Mi439-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/cipher_Mi439.mk
+
+COMMON_LUNCH_CHOICES += \
+    cipher_Mi439-user \
+    cipher_Mi439-userdebug \
+    cipher_Mi439-eng
