436f8068ac5d50b94bad4023f4390e0aa75961a6
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-04 20:54:55 +0800

    Mi439: RevengeOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_Mi439-user \
     lineage_Mi439-userdebug \
     lineage_Mi439-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_Mi439.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_Mi439-user \
+    revengeos_Mi439-userdebug \
+    revengeos_Mi439-eng
---
+++ revengeos_Mi439.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2021 The RevengeOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+# Inherit from Mi439 device
+$(call inherit-product, device/xiaomi/Mi439/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Mi439
+PRODUCT_NAME := revengeos_Mi439
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SDM439
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+
+# RevengeOS
+TARGET_BOOT_ANIMATION_RES := 720
