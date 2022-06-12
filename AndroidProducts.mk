b7b1905d45299a7494eb275f41a1b1e97ffc7794
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-13 04:18:57 +0800

    Mi439: AwakenOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_Mi439-user \
     lineage_Mi439-userdebug \
     lineage_Mi439-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/awaken_Mi439.mk
+
+COMMON_LUNCH_CHOICES += \
+    awaken_Mi439-user \
+    awaken_Mi439-userdebug \
+    awaken_Mi439-eng
