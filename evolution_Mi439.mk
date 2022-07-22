cc2a639d9e0161a78e9a801f4edb46eca1ec47e6
Sri19944 <89214859+Sri19944@users.noreply.github.com>
2022-07-22 21:00:41 +0530

    Evolution x
    Evolution x for mi439

--- evolution_Mi439.mk
+++ evolution_Mi439.mk
@@ -10,20 +10,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/Evolution-X/config/common_full_phone.mk)
+$(call inherit-product, vendor/Evolution/config/common_full_phone.mk)
 
 # Inherit from Mi439 device
-ROM := Evolution-X
+ROM := Evolution
 $(call inherit-product, device/xiaomi/Mi439/device.mk)
 
 # Overlays
 PRODUCT_PACKAGES += \
-    xiaomi_pine_overlay_Evolution-X \
-    xiaomi_olive_overlay_Evolution-X
+    xiaomi_pine_overlay_Evolution \
+    xiaomi_olive_overlay_Evolution
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi439
-PRODUCT_NAME := evolution-x_Mi439
+PRODUCT_NAME := evolution_Mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
@@ -38,5 +38,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
 
-#evolution-x
+#evolution
 TARGET_BOOT_ANIMATION_RES := 720
