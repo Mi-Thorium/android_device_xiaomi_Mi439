fbba9aba9f6d72913cb777004a01cd9c30949f5f
Sri19944 <89214859+Sri19944@users.noreply.github.com>
2022-07-27 12:27:51 +0530

    Update Evolution_Mi439.mk
    evolution os MI439

--- Evolution_Mi439.mk
+++ Evolution_Mi439.mk
@@ -13,13 +13,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, vendor/Evolution/config/common_full_phone.mk)
 
 # Inherit from Mi439 device
-rom := Evolution
+ROM := Evolution
 $(call inherit-product, device/xiaomi/Mi439/device.mk)
-
-# Overlays
-PRODUCT_PACKAGES += \
-    xiaomi_pine_overlay_lineage \
-    xiaomi_olive_overlay_lineage
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi439
@@ -37,3 +32,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+
+#Evolution
+TARGET_BOOT_ANIMATION_RES := 720
