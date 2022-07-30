e81119ee12401d894e7f217a7dfecbac8f53fbe8
Sri19944 <89214859+Sri19944@users.noreply.github.com>
2022-07-30 13:19:20 +0530

    Update and rename lineage_Mi439.mk to evolution_Mi439.mk

--- evolution_Mi439.mk
+++ evolution_Mi439.mk
@@ -9,21 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common EvolutionOS stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from Mi439 device
 PRODUCT_USE_DYNAMIC_PARTITIONS := false
+Rom := evolution
 $(call inherit-product, device/xiaomi/Mi439/device.mk)
-
-# Overlays
-PRODUCT_PACKAGES += \
-    xiaomi_pine_overlay_lineage \
-    xiaomi_olive_overlay_lineage
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi439
-PRODUCT_NAME := lineage_Mi439
+PRODUCT_NAME := evolution_Mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
