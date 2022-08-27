53959b6201336a6fd5e5b10137583cf7a1aa129c
Sri19944 <89214859+Sri19944@users.noreply.github.com>
2022-08-27 08:41:01 +0530

    Update evolution_Mi439.mk

--- evolution_Mi439.mk
+++ evolution_Mi439.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Evolution OS stuff.
+# Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from Mi439 device
