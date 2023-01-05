eb030ec12a5f03d992d130cd56d14c3b30cae222
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-05 23:48:42 +0800

    Mi439: rootdir: Disable QTI Gatekeeper & Keymaster

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,6 @@ TARGET_OTA_ASSERT_DEVICE := pine,olive,olivelite,olivewood,olives,mi439,Mi439,Mi
 TARGET_SCREEN_DENSITY := 320
 
 # HIDL
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest/gatekeeper.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Init
