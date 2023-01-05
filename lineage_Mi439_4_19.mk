d5ffd651935db538424128205d70ae30bb210ce7
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-06 00:16:23 +0800

    Mi439: State that it's test build

--- lineage_Mi439_4_19.mk
+++ lineage_Mi439_4_19.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+TARGET_UNOFFICIAL_BUILD_ID := TEST
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
