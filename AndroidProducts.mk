9d63972b7f6dc0e99569ab9296a61ad7404153a7
me-cafebabe <me.cafebabe@gmail.com>
2023-01-30 01:06:47 +0800

    Miku: Mi439: Miku-UI TDA Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,17 +1,17 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 Miku-UI
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi439.mk \
-    $(LOCAL_DIR)/lineage_Mi439_4_19.mk
+    $(LOCAL_DIR)/miku_Mi439.mk \
+    $(LOCAL_DIR)/miku_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng \
-    lineage_Mi439_4_19-user \
-    lineage_Mi439_4_19-userdebug \
-    lineage_Mi439_4_19-eng
+    miku_Mi439-user \
+    miku_Mi439-userdebug \
+    miku_Mi439-eng \
+    miku_Mi439_4_19-user \
+    miku_Mi439_4_19-userdebug \
+    miku_Mi439_4_19-eng
--- miku_Mi439.mk
+++ miku_Mi439.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 Miku-UI
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,11 +9,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Miku stuff.
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 # Kernel
-TARGET_KERNEL_VERSION := 4.19
+TARGET_KERNEL_VERSION := 4.9
 
 # Inherit from Mi439 device
 $(call inherit-product, device/xiaomi/Mi439/device.mk)
@@ -24,8 +24,8 @@ PRODUCT_PACKAGES += \
     xiaomi_olive_overlay_lineage
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Mi439_4_19
-PRODUCT_NAME := lineage_Mi439_4_19
+PRODUCT_DEVICE := Mi439
+PRODUCT_NAME := miku_Mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
