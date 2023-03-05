ec0f2086a3dc3fbda6ba6a1eaf1942ef212dfc3d
me-cafebabe <me.cafebabe@gmail.com>
2023-03-05 20:28:26 +0800

    Mi439: Copy hvdcp_opti to recovery
    * It's responsible for reporting correct battery capacity, at least.
    * The newly added charger_led service needs correct battery capacity
    
    Change-Id: I3a14e02f8b857a8007a34fbbe902949ead3373a3

--- device.mk
+++ device.mk
@@ -63,6 +63,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Recovery
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/Mi439/proprietary/vendor/bin/hvdcp_opti:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/hvdcp_opti
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom_ramdisk \
