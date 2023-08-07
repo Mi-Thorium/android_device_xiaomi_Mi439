40e2f806a84c5312b7f68082883efb6ab8de407f
me-cafebabe <me.cafebabe@gmail.com>
2023-08-07 22:45:08 +0800

    Mi8937: Add LiveDisplay CABC & CE & HBM support through sysfs
    Change-Id: I20360b58cee27f1a0e30b082136730ae2261323b

--- device.mk
+++ device.mk
@@ -68,6 +68,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Recovery
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/Mi439/proprietary/vendor/bin/hvdcp_opti:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/hvdcp_opti
